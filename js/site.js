e8b82eba4db18b3f8edf75810d7541c67ed44590
eprzystal <eprzystal@hawk.iit.edu>
2021-03-16 14:03:21 -0500

    script moved on html

--- js/site.js
+++ js/site.js
@@ -1,6 +1,10 @@
 // jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
 
-$('.luck').click(function(){
+$("#luck").click(function(){
     if($(this).hasClass('active')){
         $(this).removeClass('active')
     } else {
