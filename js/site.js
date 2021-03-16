470e998d52e8ad73e5b13b2f2e86e0a2f3941987
eprzystal <eprzystal@hawk.iit.edu>
2021-03-16 13:55:57 -0500

    modifying js link

--- js/site.js
+++ js/site.js
@@ -1,5 +1,9 @@
 // jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
+
+$('.luck').click(function(){
+    if($(this).hasClass('active')){
+        $(this).removeClass('active')
+    } else {
+        $(this).addClass('active')
+    }
 });
