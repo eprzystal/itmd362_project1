25b61e1218906dcb315a28726a40010a56449345
eprzystal <eprzystal@hawk.iit.edu>
2021-03-16 11:07:41 -0500

    updated js

--- js/site.js
+++ js/site.js
@@ -1,11 +1,5 @@
- // jQuery 3.x-style ready event and locally scoped $
+// jQuery 3.x-style ready event and locally scoped $
 jQuery(function($) {
-    var img = document.getElementById('blinking_cow');
-
-var interval = window.setInterval(function(){
-    if(img.style.visibility == 'hidden'){
-        img.style.visibility = 'visible';
-    }else{
-        img.style.visibility = 'hidden';
-    }
-}, 1000); //the 1000 here is milliseconds and determines how often the interval should be run.
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
