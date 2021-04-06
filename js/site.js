5a7a9b45ac8f923957d76c80f873700213544394
eprzystal <eprzystal@hawk.iit.edu>
2021-04-06 16:23:31 -0500

    updated JS

--- js/site.js
+++ js/site.js
@@ -1,7 +1,5 @@
 // jQuery 3.x-style ready event and locally scoped $
 
-document.getElementById("chance").addEventListener("return", myFunction);
-
 function myFunction() {
-  document.getElementById("chance").innerHTML = "WOW, WINNER!";
+  alert('Congratulations!  You are a Winner!');
 }
